59d27461db551a8a40f692ec46c5f6463dc6ad81
Sosnina Sasha <aleksandra.sosnina@unitbean.com>
2020-10-13 18:38:32 +0400

    towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+function towelSort(matrix) {
+
+  if (!matrix) {
+    return [];
+  }
+
+
+    let result = [];
+
+
+    for (let i = 0; i < matrix.length; i += 1) {
+
+        if (i % 2 != 0) {
+            result = result.concat(matrix[i].reverse());
+        } else {
+            result = result.concat(matrix[i]);
+        }
+
+        console.log(result);
+    }
+    return result;
 }
+
+module.exports = towelSort;
